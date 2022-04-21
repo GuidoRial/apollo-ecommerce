50dce62f967440aceacbdc32e686d635d77f20ca
GuidoRial <rialguido@gmail.com>
2022-04-21 13:14:47 -0300

    added function to save current product to state

--- client/src/components/ProductDescriptionPage/ProductDescriptionPage.js
+++ client/src/components/ProductDescriptionPage/ProductDescriptionPage.js
@@ -8,49 +8,39 @@ export default class ProductDescriptionPage extends Component {
         this.state = {
             individualProduct: {},
         };
+        this.getId = this.getId.bind(this);
         this.fetchProduct = this.fetchProduct.bind(this);
     }
-    fetchProduct = async (id) => {
+    fetchProduct = async (productId) => {
         const result = await client
             .query({
                 query: getProductById,
                 variables: {
-                    id: id,
+                    id: productId,
                 },
             })
             .then((result) => {
-                console.log(result.data);
+                this.setState({ individualProduct: result.data.product });
             });
     };
-
-    componentDidMount() {
-        const getId = () => {
-            /* 
+    getId = () => {
+        /* 
             useParams because functional components are not allowed.
             I thought about using a Higher Order Component but by definition they are a function that takes a component as a parameter and return a component, thus making it a functional component which is not allowed.
             I considered using withRouter (https://reactrouter.com/docs/en/v6/faq#what-happened-to-withrouter-i-need-it) but I wont because of the reasons disclosed above.
             
             My solution to this problem was to isolate the id by manipulating the window.location object until it always return whatever is after /products/, therefore, returning the product id
             */
-            const thisPathname = window.location.pathname
-                .toString()
-                .substring(10);
-            console.log(thisPathname);
-        };
+        const idFromURL = window.location.pathname.toString().substring(10);
 
-        getId();
+        return idFromURL;
+    };
+    componentDidMount() {
+        this.fetchProduct(this.getId());
     }
 
-    componentWillUnmount() {}
-
     render() {
-
-
-        /*
-        
-    1. Import data for individual product with ID
-    2. Render
-    */
+        console.log(this.state.individualProduct);
 
         return <div>ProductDescriptionPage</div>;
     }
